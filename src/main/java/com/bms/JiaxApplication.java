a7380a90e3641d616fdfc7c2d58a038bc7fe0bb8
jiax <1137851998@qq.com>
2022-10-04 00:24:06 +0800

    2

--- src/main/java/com/bms/JiaxApplication.java
+++ src/main/java/com/bms/JiaxApplication.java
@@ -3,7 +3,7 @@ package com.bms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-//test
+//test2
 @SpringBootApplication
 @EnableScheduling
 public class JiaxApplication {
