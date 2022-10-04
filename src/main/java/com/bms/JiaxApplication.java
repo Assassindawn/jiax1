2888ab492b6436bf0180e52319d532474b8e05ad
jiax <1137851998@qq.com>
2022-10-04 11:08:28 +0800

    4 version

--- src/main/java/com/bms/JiaxApplication.java
+++ src/main/java/com/bms/JiaxApplication.java
@@ -3,7 +3,7 @@ package com.bms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-//test3
+//test4
 @SpringBootApplication
 @EnableScheduling
 public class JiaxApplication {
